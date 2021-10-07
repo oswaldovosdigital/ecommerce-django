5ef227e4ab458c7ba4ddad9de929d17b57374f84
oswaldovosdigital <vosdigital2021@gmail.com>
2021-10-07 15:49:12 -0500

    updated postgres

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -91,14 +91,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 AUTH_USER_MODEL = 'accounts.Account' #es para definir la estructura de usuasrios
 
 
-DATABASES = {
-        'default': {
-           'ENGINE': 'django.db.backends.sqlite3',
-           'NAME': BASE_DIR / 'db.sqlite3',
-       }
-    }
 
-# Database
+#Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 #DATABASES = {
 #    'default': {
@@ -111,24 +105,24 @@ DATABASES = {
 #    }
 #}
 
-#if 'RDS_DB_NAME' in os.environ:
-#    DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.postgresql',
-#            'NAME': os.environ['RDS_DB_NAME'],
-#            'USER': os.environ['RDS_USERNAME'],
-#            'PASSWORD': os.environ['RDS_PASSWORD'],
-#            'HOST': os.environ['RDS_HOSTNAME'],
-#            'PORT': os.environ['RDS_PORT'],
-#        }
-#    }
-#else:
-#    DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.sqlite3',
-#            'NAME': BASE_DIR / 'db.sqlite3',
-#        }
-#    }
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
